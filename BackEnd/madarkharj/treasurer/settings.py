6d244704e8f8e8a7b5dc891e33b1dae0fd6c6038
MahdiHedayati01 <mr.mahdihedayatii@gmail.com>
2024-06-04 13:31:30 +0330

    vercel cofiguration removed part2

--- BackEnd/madarkharj/treasurer/settings.py
+++ BackEnd/madarkharj/treasurer/settings.py
@@ -119,8 +119,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn", "static_root")
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "static_cdn", "media_root")
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "assets")
+]
 
 
 # Default primary key field type
--- BackEnd/madarkharj/treasurer/urls.py
+++ BackEnd/madarkharj/treasurer/urls.py
@@ -17,8 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
