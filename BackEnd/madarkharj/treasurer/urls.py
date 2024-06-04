89bfc8ed878946affcf03bf5a9fa7d2b2ff57ae0
MahdiHedayati01 <mr.mahdihedayatii@gmail.com>
2024-06-04 13:28:02 +0330

    vercel configuration removed

--- BackEnd/madarkharj/treasurer/urls.py
+++ BackEnd/madarkharj/treasurer/urls.py
@@ -25,7 +25,4 @@ urlpatterns = [
     path('', include('users.api.urls')),
     path('api-auth/', include('rest_framework.urls')),
 ]
-# add at the last
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
